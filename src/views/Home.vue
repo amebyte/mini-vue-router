5dd188781ba17d1b91d7a41eaa01e809ccf9a591
unknown <416212316@qq.com>
2021-10-13 00:01:03 +0800

    computed

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,18 @@
 <template>
   <div class="home">
     Home page
+    <p v-for="(item, key) in data" :key="key">{{item}}</p>
   </div>
 </template>
 
 <script>
+const data = [1,2,3]
 export default {
-  name: 'Home'
+  name: 'Home',
+  computed: {
+      data() {
+          return data
+      }
+  }
 }
 </script>
